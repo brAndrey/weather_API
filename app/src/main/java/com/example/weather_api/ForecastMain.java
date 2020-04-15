3b65cce51d2a314cbd383c79139fea0bee4ecbfe
andrey_vb <train128412@gmail.ru>
2020-04-15 15:48:23 +0300

    Forecast Main

---
+++ app/src/main/java/com/example/weather_api/ForecastMain.java
@@ -0,0 +1,30 @@
+package com.example.weather_api;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ForecastMain {
+
+    private float temp;
+
+    private float pressure;
+
+    private  float himidity;
+
+    @SerializedName("temp_min")
+    private float minTemp;
+
+    @SerializedName("temp_max")
+    private float maxTemp;
+
+    // ************************************
+    public  float getTemp(){ return temp;}
+
+    public  float getPressure (){return pressure;}
+
+    public  float getHimidity(){return himidity;}
+
+    public  float getMinTemp(){ return minTemp;}
+
+    public  float getMaxTemp(){return maxTemp;}
+
+}
